2d6d89ad4f5004f5a802a5b925f430be326f171f
wallace ferreira <ze.da.jr@gmail.com>
2025-02-20 22:05:16 -0300

    inicio da solicitacao

--- src/components/solicitacao/Solicitacao.jsx
+++ src/components/solicitacao/Solicitacao.jsx
@@ -1,8 +1,71 @@
-function Solicitacao (){
-    return(
-        <>
-        <h2>eu sou o componente Solicitacao</h2>
-        </>
-    )
+import NavBar from "../navbar/NavBar.jsx";
+import styles from "./Solicitacao.module.scss";
+import Home from "../../assets/Dashboard/homeHeader.png";
+import Seta from "../../assets/Dashboard/Vector.png";
+function Solicitacao() {
+  return (
+    <div className={styles.layoutSolicitacao}>
+      <NavBar />
+      <div className={styles.containerPrincipalSolicitacao}>
+        <header className={styles.headerSolicitacao}>
+          <img src={Home} alt="vetor da casinha" />
+          <img src={Seta} alt="vetor da setinha" />
+          <p>Reembolsos</p>
+          <img src={Seta} alt="vetor da setinha" />
+          <p>Solicitação de Reembolso</p>
+        </header>
+
+        <main className={styles.mainSolicitacao}>
+          <form className={styles.formSolicitacao}>
+            <div className={styles.grupo1}>
+              <div className={styles.inputNome}>
+                <label htmlFor="">Nome Completo</label>
+                <input type="text" />
+              </div>
+
+              <div className={styles.inputEmpresa}>
+                <label htmlFor="">Empresa</label>
+                <input type="text" />
+              </div>
+
+              <div className={styles.inputPrestacao}>
+                <label htmlFor="">Nº Prest. Contas</label>
+                <input type="number" name="" id="" />
+              </div>
+
+              <div className={styles.inputMotivo}>
+                <label htmlFor="">Descrição / Motivo do Reembolso</label>
+                <textarea name="" id="">
+                  {" "}
+                </textarea>
+              </div>
+            </div>
+
+            <div className={styles.barraVertical}></div>
+
+            <div className={styles.grupo2}>
+              <div className={styles.inputData}>
+                <label htmlFor="">Data</label>
+                <input type="date" name="" id="" />
+              </div>
+
+              <div className={styles.despesas}>
+                <label htmlFor=""> Tipo de Despesa</label>
+                
+                <select name="" id="">
+                  <option value="">Selecionar </option>
+                  <option value=""> Alimentação</option>
+                  <option value="">Combustível</option>
+                </select>
+              </div>
+
+
+
+            </div>
+          </form>
+        </main>
+      </div>
+    </div>
+  );
 }
-export default Solicitacao
+export default Solicitacao;
