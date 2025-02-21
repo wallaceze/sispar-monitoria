708290e1695cb1493d02828ac042e39b2c6b9575
wallace ferreira <ze.da.jr@gmail.com>
2025-02-21 20:24:56 -0300

    não terminado ainda

--- src/components/solicitacao/Solicitacao.jsx
+++ src/components/solicitacao/Solicitacao.jsx
@@ -1,7 +1,9 @@
 import NavBar from "../navbar/NavBar.jsx";
 import styles from "./Solicitacao.module.scss";
-import Home from "../../assets/Dashboard/homeHeader.png";
+import Home from "../../assets/Dashboard/homeheader.png";
 import Seta from "../../assets/Dashboard/Vector.png";
+import Lixeira from "../../components/solicitacao/lixo.png";
+import Motivo from "../../components/solicitacao/motivo.png";
 function Solicitacao() {
   return (
     <div className={styles.layoutSolicitacao}>
@@ -51,18 +53,136 @@ function Solicitacao() {
 
               <div className={styles.despesas}>
                 <label htmlFor=""> Tipo de Despesa</label>
-                
+
                 <select name="" id="">
-                  <option value="">Selecionar </option>
-                  <option value=""> Alimentação</option>
-                  <option value="">Combustível</option>
+                  <option value="">Selecionar</option>
+                  <option value=""> Alimentação </option>
+                  <option value=""> Combustível</option>
+                  <option value=""> Condução</option>
+                  <option value=""> Estacionamento</option>
+                  <option value=""> Viagem Admin.</option>
+                  <option value=""> Viagem Operacional</option>
+                  <option value=""> Eventos de representação</option>
                 </select>
               </div>
 
+              <div className={styles.centroDeCusto}>
+                <label htmlFor="">Centro de Custo</label>
+                <select name="" id="">
+                  <option value=""> Selecionar</option>
+                  <option value="">
+                    1100109002 - FIN CONTROLES INTERNOS MTZ
+                  </option>
+                  <option value="">
+                    1100110002 - FIN VICE-PRESIDENCIA FINANCAS MTZ
+                  </option>
+                  <option value="">1100110101 - FIN CONTABILIDADE MTZ</option>
+                </select>
+              </div>
 
+              <div className={styles.ordem}>
+                <label htmlFor=""> Ord. Int.</label>
+                <input type="number" name="" id="" />
+              </div>
 
+              <div className={styles.divisoes}>
+                <label htmlFor="">Div.</label>
+                <input type="number" name="" id="" />
+              </div>
+
+              <div className={styles.pep}>
+                <label htmlFor="">PEP</label>
+                <input type="number" name="" id="" />
+              </div>
+
+              <div className={styles.moeda}>
+                <label htmlFor="">Moeda</label>
+                <select name="" id="">
+                  <option value="">Selecionar</option>
+                  <option value="">BRL</option>
+                  <option value="">ARS</option>
+                  <option value="">USD</option>
+                </select>
+              </div>
+
+              <div className={styles.distancia}>
+                <label htmlFor="">Dist / Km</label>
+                <input type="text" />
+              </div>
+
+              <div className={styles.valorKm}>
+                <label htmlFor="">Valor / Km</label>
+                <input type="number" name="" id="" />
+              </div>
+
+              <div className={styles.valorFaturado}>
+                <label htmlFor="">Val. Faturado</label>
+                <input type="number" name="" id="" />
+              </div>
+
+              <div className={styles.despesa}>
+                <label htmlFor="">Despesa</label>
+                <input type="number" name="" id="" />
+              </div>
+
+              <div className={styles.botoes}>
+                <button className={styles.botaoSalvar}> + Salvar</button>
+
+                <button className={styles.botaoDeletar}> Del</button>
+              </div>
             </div>
           </form>
+
+          {/* table é a tag principal que vai definir a tabela */}
+          {/* thred é a tag que agrupa o cabeçalho */}
+          {/* tr é a linha da tabela */}
+          {/* th é o título da tabela, um para cada título. Ex: Nome - idade - cor - fruta */}
+          {/* tbody é a tag que agrupa o corpo da tabela */}
+          {/* td é a tag que representa uma célula */}
+
+          <table>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Colaborador(a)</th>
+                <th>Empresa</th>
+                <th>Nº Prest.</th>
+                <th>Data</th>
+                <th>Motivo</th>
+                <th>Tipo de despesa</th>
+                <th>Ctr. Custo</th>
+                <th>Ord. Int.</th>
+                <th>Div.</th>
+                <th>PEP</th>
+                <th>Moeda</th>
+                <th>Dist. Km</th>
+                <th>Val. Km</th>
+                <th>Val. Faturado</th>
+                <th>Despesa</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td> <img src={Lixeira} alt="ícone da lixeira" /></td>
+                <td> Cleitinho Caramelo</td>
+                <td> Mototáxi</td>
+                <td> 24</td>
+                <td> 21/02/2025</td>
+                <td> <img src={Motivo} alt="" /></td>
+                <td> Alimentação</td>
+                <td> 200.00</td>
+                <td>1</td>
+                <td>0</td>
+                <td>nada</td>
+                <td>BRL</td>
+                <td>20km</td>
+                <td>0.50</td>
+                <td>600.00</td>
+                <td>1500.00</td>
+              </tr>
+            </tbody>
+          </table>
         </main>
       </div>
     </div>
